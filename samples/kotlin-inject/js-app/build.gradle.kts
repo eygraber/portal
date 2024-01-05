e55e29ca31793d1208d8a0a7004b51b2e307ed7f
Eliezer Graber <me@eygraber.com>
2024-01-05 04:44:52 -0500

    Ignore default targets from the js samples

--- samples/kotlin-inject/js-app/build.gradle.kts
+++ samples/kotlin-inject/js-app/build.gradle.kts
@@ -14,9 +14,9 @@ kotlin {
     project = project,
     binaryType = BinaryType.Executable,
     webOptions = KmpTarget.WebOptions(
-      isNodeEnabled = true,
       moduleName = "kotlin-inject",
     ),
+    ignoreDefaultTargets = true,
   )
 
   sourceSets {
