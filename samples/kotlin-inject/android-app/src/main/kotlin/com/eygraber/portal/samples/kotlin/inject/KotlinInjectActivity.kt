a187b90947a1b52689f4c5e836562790980bc3fe
Eliezer Graber <eygraber@gmail.com>
2024-05-08 15:40:24 -0700

    Remove androidx.appcompat as it is not needed (#686)

--- samples/kotlin-inject/android-app/src/main/kotlin/com/eygraber/portal/samples/kotlin/inject/KotlinInjectActivity.kt
+++ samples/kotlin-inject/android-app/src/main/kotlin/com/eygraber/portal/samples/kotlin/inject/KotlinInjectActivity.kt
@@ -1,10 +1,10 @@
 package com.eygraber.portal.samples.kotlin.inject
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 
-class KotlinInjectActivity : AppCompatActivity() {
+class KotlinInjectActivity : ComponentActivity() {
   private val mainPortal = appComponent.mainPortal
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
